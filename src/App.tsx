1a5b42f08920c8d8747600178efef45e5edb144d
Kostya Lapatin <klrotex11@gmail.com>
2022-08-28 22:43:11 +0300

    Timer with fixed value

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Counter} from "./components/counter/Counter";
+import {Counter} from "./components/Counter/Counter";
+
 
 function App() {
+
+    const start = 0
+    const end = 5
+
+    const [count, setCount] = useState<number>(start)
+
+    const plusCount = () => {
+        if (count < end) {
+            setCount(count + 1)
+        }
+    }
+
+    const resetCount = () => {
+        setCount(0)
+    }
+
     return (
-        <div className={'app'}>
-            <Counter/>
+        <div className={'wrapper'}>
+            <Counter
+                start={start}
+                end={end}
+                title={count}
+                plusCount={plusCount}
+                resetCount={resetCount}
+            />
         </div>
     )
 }
